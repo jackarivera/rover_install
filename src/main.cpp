521c5019b130e67cba7c67fc41d68d19a69535c5
Jack Rivera <jackarivera@gmail.com>
2023-03-22 22:00:12 -0400

    Basic UI Layout and Gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,82 @@
 #include <iostream>
+#include <vector>   // for vector
+#include <string>
 
-#include "ftxui/dom/elements.hpp"
-#include "ftxui/screen/screen.hpp"
-#include "ftxui/screen/string.hpp"
+#include "ftxui/component/component.hpp"  // for Checkbox, Renderer, Horizontal, Vertical, Input, Menu, Radiobox, ResizableSplitLeft, Tab
+#include "ftxui/component/component_base.hpp"  // for ComponentBase, Component
+#include "ftxui/component/component_options.hpp"  // for MenuOption, InputOption
+#include "ftxui/component/event.hpp"              // for Event, Event::Custom
+#include "ftxui/component/screen_interactive.hpp"  // for Component, ScreenInteractive
+#include "ftxui/dom/elements.hpp"  // for text, color, operator|, bgcolor, filler, Element, vbox, size, hbox, separator, flex, window, graph, EQUAL, paragraph, WIDTH, hcenter, Elements, bold, vscroll_indicator, HEIGHT, flexbox, hflow, border, frame, flex_grow, gauge, paragraphAlignCenter, paragraphAlignJustify, paragraphAlignLeft, paragraphAlignRight, dim, spinner, LESS_THAN, center, yframe, GREATER_THAN
+#include "ftxui/dom/flexbox_config.hpp"  // for FlexboxConfig
+#include "ftxui/screen/color.hpp"  // for Color, Color::BlueLight, Color::RedLight, Color::Black, Color::Blue, Color::Cyan, Color::CyanLight, Color::GrayDark, Color::GrayLight, Color::Green, Color::GreenLight, Color::Magenta, Color::MagentaLight, Color::Red, Color::White, Color::Yellow, Color::YellowLight, Color::Default, Color::Palette256, ftxui
+#include "ftxui/screen/color_info.hpp"  // for ColorInfo
+#include "ftxui/screen/terminal.hpp"    // for Size, Dimensions
+#include "ftxui/dom/canvas.hpp"
+
+using namespace ftxui;
 
 int main(void) {
-  using namespace ftxui;
-
-  auto summary = [&] {
-    auto content = vbox({
-        hbox({text(L"- done:   "), text(L"3") | bold}) | color(Color::Green),
-        hbox({text(L"- active: "), text(L"2") | bold}) | color(Color::RedLight),
-        hbox({text(L"- queue:  "), text(L"9") | bold}) | color(Color::Red),
-    });
-    return window(text(L" Summary "), content);
+  const std::vector<std::string> robot_list = {
+    "Mini",
+    "Miti",
+    "Zero2",
+    "Zero3",
+    "Pro"
   };
 
-  auto document =  //
-      vbox({
-          hbox({
-              summary(),
-              summary(),
-              summary() | flex,
-          }),
-          summary(),
-          summary(),
-      });
+  std::array<std::string, 7> additional_packages = {
+    "Foxglove",
+    "Slam Toolbox",
+    "Nav2",
+    "Slamtec Lidar Driver",
+    "BMI088 IMU Driver",
+    "BNO055 IMU Driver",
+    "Intel Realsense Driver"
+  };
 
-  // Limit the size of the document to 80 char.
-  document = document | size(WIDTH, LESS_THAN, 80);
+  std::array<bool, 7> additional_packages_states = {
+    true,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  };
 
-  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
-  Render(screen, document);
+  int selected_robot = 0;
+  Component robots_component = Radiobox(&robot_list, &selected_robot);
 
-  std::cout << screen.ToString() << '\0' << std::endl;
+  Component packages_component = Container::Vertical({
+      Checkbox(&additional_packages[0], &additional_packages_states[0]),
+      Checkbox(&additional_packages[1], &additional_packages_states[1]),
+      Checkbox(&additional_packages[2], &additional_packages_states[2]),
+      Checkbox(&additional_packages[3], &additional_packages_states[3]),
+      Checkbox(&additional_packages[4], &additional_packages_states[4]),
+      Checkbox(&additional_packages[5], &additional_packages_states[5]),
+      Checkbox(&additional_packages[6], &additional_packages_states[6])
+  });
+
+  auto layout = Container::Horizontal({
+    robots_component,
+    packages_component
+  });
+
+  auto installer_renderer = Renderer(layout, [&] {
+    auto robots_win = window(text("Rover Robots"), robots_component->Render() | frame | size(WIDTH, EQUAL, 20));
+    auto packages_win = window(text("Additional Packages"), packages_component->Render() | frame);
+
+    return hbox({
+              robots_win,
+              packages_win
+    });
+  });
+  
+
+  auto screen = ScreenInteractive::TerminalOutput();
+  system("clear");
+  screen.Loop(installer_renderer);
 
   return EXIT_SUCCESS;
 }
